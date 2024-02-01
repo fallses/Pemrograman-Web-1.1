69e32b67b71cb1f6efc660747aac3431aed22811
fallses <naufal.zydan05@gmail.com>
2024-02-01 07:45:54 +0700

    first upload

---
+++ operator2.php
@@ -0,0 +1,19 @@
+<?php
+    $x=10;
+    $y=6;
+    echo ($x + $y);
+    echo "<br>";
+    echo ($x- $y);
+    echo "<br>";
+    echo ($x * $y);
+    echo "<br>";
+    echo ($x / $y);
+    echo "<br>";
+    echo ($x % $y);
+    $a = "Hello";
+    $b = $a . " world!";
+    echo "<br>";
+    echo $b;
+    echo "<br>";
+    echo $a. " -- " . $b. " ini string operator ";
+?>
---
+++ variabel.php
@@ -0,0 +1,12 @@
+<?php
+    $nim="E32232415";
+    $nama="Naufal";
+    $prodi="Teknik Informatika";
+    echo "Selamat Praktikum";
+    echo "<br/>";
+    echo $nim;
+    echo "<br/>";
+    echo $nama;
+    echo "<br/>";
+    echo $prodi
+?>
